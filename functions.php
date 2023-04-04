512e4a96ea85d95c65a69b95f3e0734d42ae4883
Jenn Schlick <jlschlick@gmail.com>
2023-04-04 09:42:38 -0400

    Remove legacy theme.

--- functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*-----------------------------------------------------------------------------------*/
-/* Load function partials from /functions/*.php
-/*-----------------------------------------------------------------------------------*/
-
-foreach( glob( get_template_directory() . "/functions/*.php" ) as $file ) {
-  require $file;
-}
