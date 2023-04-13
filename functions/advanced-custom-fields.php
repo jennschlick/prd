c35ea7ffd83b9ff555751e213da790ade6f4dd65
Jenn Schlick <jlschlick@gmail.com>
2023-04-13 18:49:59 -0400

    Update ACF functions.

--- functions/advanced-custom-fields.php
+++ functions/advanced-custom-fields.php
@@ -3,8 +3,12 @@
 /* Advanced Custom Fields plugin functions
 /*-----------------------------------------------------------------------------------*/
 
+// Save field groups
 function prd_json_save_point( $path ) {
 	$path = get_stylesheet_directory() . '/acf-json';
 	return $path;
 }
 add_filter( 'acf/settings/save_json', 'prd_json_save_point' );
+
+// Allow shortcodes in text fields
+add_filter( 'acf/format_value/type=text', 'do_shortcode' );
