46a1a09f37f4e01a06087d406a49e3f3a1a4444c
Jenn Schlick <jlschlick@gmail.com>
2023-04-14 15:40:31 -0400

    Add Google Maps API key.

--- functions/advanced-custom-fields.php
+++ functions/advanced-custom-fields.php
@@ -12,3 +12,10 @@ add_filter( 'acf/settings/save_json', 'prd_json_save_point' );
 
 // Allow shortcodes in text fields
 add_filter( 'acf/format_value/type=text', 'do_shortcode' );
+
+// Add Google Maps API key
+function prd_google_map_api( $api ) {
+	$api['key'] = 'AIzaSyD_lVoOEI5WQCi6I-R0braUkVEXZCmvcL8';
+	return $api;   
+}
+add_filter( 'acf/fields/google_map/api', 'prd_google_map_api' );
