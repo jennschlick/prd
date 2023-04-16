d0adb946804695beb8c67cb89a91c22f602cac0a
Jenn Schlick <jlschlick@gmail.com>
2023-04-16 11:44:10 -0400

    Update scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
 // Open external links in a new tab
 window.addEventListener("DOMContentLoaded", e => {
-  document.querySelectorAll('a[href]').forEach(a => {
-    if (location.hostname == new URL(a.href).hostname)
-      return;
-    a.target = "_blank";
-    a.rel = "noreferrer nofollow noopener";
-  });
+	document.querySelectorAll('a[href]').forEach(a => {
+		if (location.hostname == new URL(a.href).hostname)
+			return;
+		a.target = "_blank";
+		a.rel = "noreferrer nofollow noopener";
+	});
 });
 
-// (function($){
+(function($) {
 
-// 	$(document).ready(function(){
-		
-// 	});
+	// Add class to header on scroll
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1) {
+			$('.header').addClass("sticky");
+		}
+		else {
+			$('.header').removeClass("sticky");
+		}
+	});
 
-// })(jQuery)
+})( jQuery );
+
+
