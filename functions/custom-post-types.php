138c815a794ab0ebd68327791ad2c79637bffe3d
Jenn Schlick <jlschlick@gmail.com>
2023-04-17 15:48:32 -0400

    Update custom post types.

--- functions/custom-post-types.php
+++ functions/custom-post-types.php
@@ -5,45 +5,16 @@
 
 function prd_custom_post_types() {
 
-	// People
-	$labels = array(
-		'name' => __('People'),
-		'singular_name' => __('Person'),
-		'add_new' => __('Add New'),
-		'add_new_item' => __('Add Person'),
-		'edit_item' => __('Edit Person'),
-		'new_item' => __('Add Person'),
-		'view_item' => __('View Person'),
-		'search_items' => __('Search People'),
-		'not_found' =>  __('Nothing found'),
-		'not_found_in_trash' => __('Nothing found'),
-		'parent_item_colon' => ''
-	);
-	$args = array(
-		'labels' => $labels,
-		'public' => false,
-		'publicly_queryable' => true,
-		'menu_icon' => 'dashicons-admin-page',
-		'show_ui' => true,
-		'query_var' => false,
-		'capability_type' => 'post',
-		'hierarchical' => false,
-		'show_in_rest' => true,
-		'supports' => array('title','editor','thumbnail','revisions'),
-		'has_archive' => false
-	);
-	register_post_type('person', $args);
-
 	// Projects
 	$labels = array(
 		'name' => __('Projects'),
 		'singular_name' => __('Project'),
 		'add_new' => __('Add New'),
-		'add_new_item' => __('Add Project'),
-		'edit_item' => __('Edit Project'),
-		'new_item' => __('Add Project'),
-		'view_item' => __('View Project'),
-		'search_items' => __('Search Projects'),
+		'add_new_item' => __('Add'),
+		'edit_item' => __('Edit'),
+		'new_item' => __('Add'),
+		'view_item' => __('View'),
+		'search_items' => __('Search'),
 		'not_found' =>  __('Nothing found'),
 		'not_found_in_trash' => __('Nothing found'),
 		'parent_item_colon' => ''
@@ -63,5 +34,35 @@ function prd_custom_post_types() {
 		'has_archive' => false
 	);
 	register_post_type('project', $args);
+
+	// Team
+	$labels = array(
+		'name' => __('Team'),
+		'singular_name' => __('Team member'),
+		'add_new' => __('Add New'),
+		'add_new_item' => __('Add'),
+		'edit_item' => __('Edit'),
+		'new_item' => __('Add'),
+		'view_item' => __('View'),
+		'search_items' => __('Search'),
+		'not_found' =>  __('Nothing found'),
+		'not_found_in_trash' => __('Nothing found'),
+		'parent_item_colon' => ''
+	);
+	$args = array(
+		'labels' => $labels,
+		'public' => false,
+		'publicly_queryable' => true,
+		'menu_icon' => 'dashicons-admin-page',
+		'menu_position'=> 27,
+		'show_ui' => true,
+		'query_var' => false,
+		'capability_type' => 'post',
+		'hierarchical' => false,
+		'show_in_rest' => true,
+		'supports' => array('title','editor','thumbnail','revisions'),
+		'has_archive' => false
+	);
+	register_post_type('team', $args);
 }
 add_action('init', 'prd_custom_post_types');
