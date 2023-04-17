3c6fda99926e921705b02ad0073b8c00992164ea
Jenn Schlick <jlschlick@gmail.com>
2023-04-17 17:14:07 -0400

    Customize the login page.

--- functions/admin.php
+++ functions/admin.php
@@ -3,6 +3,26 @@
 /* Admin functions
 /*-----------------------------------------------------------------------------------*/
 
+// Customize the login page
+function prd_login_logo() { ?>
+<style type="text/css">
+#login h1 a,
+.login h1 a {
+	background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/logo.svg);
+	height: 80px;
+	width: 100%;
+	background-size: contain;
+	background-repeat: no-repeat;
+	padding-bottom: 30px;
+}
+</style>
+<?php } add_action( 'login_enqueue_scripts', 'prd_login_logo' );
+
+function prd_login_logo_url() {
+  return home_url();
+}
+add_filter( 'login_headerurl', 'prd_login_logo_url' );
+
 // Remove dashboard welcome panel
 remove_action('welcome_panel', 'wp_welcome_panel');
 
