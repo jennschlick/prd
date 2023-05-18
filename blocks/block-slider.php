76395e5ac2c61e4948427445c5a24c046b024d57
Jenn Schlick <jlschlick@gmail.com>
2023-05-18 11:08:24 -0400

    Decrease slider speed.

--- blocks/block-slider.php
+++ blocks/block-slider.php
@@ -1,5 +1,5 @@
 <div class="slider-wrapper">
-  <div class="slider" data-flickity='{ "adaptiveHeight": true, "autoPlay": true, "cellAlign": "left", "draggable": true, "prevNextButtons": false, "wrapAround": true }'>
+  <div class="slider" data-flickity='{ "adaptiveHeight": true, "autoPlay": 3500, "cellAlign": "left", "draggable": true, "prevNextButtons": false, "wrapAround": true }'>
     <?php block_field( 'block-slider-content' ); ?>
   </div>
 </div>
